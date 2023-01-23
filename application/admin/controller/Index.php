5a1210cb3bd44e2675ab5d0488b422e805015364
huicat28 <huicat28@163.com>
2023-01-23 14:18:41 +0800

    基础管理员功能上线

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -2,12 +2,24 @@
 namespace app\admin\controller;
 use think\Db;
 use think\Controller;
+use think\facade\Session;
+
 class Index extends Controller
 {
     public function index()
     {
-        $this->assign("SETTING_SITENAME", Db::table("icp_settings")->where("key_name", "site_name")->value("key_value"));
-        $this->assign("PAGE_TITLE", "管理员主页");
-        exit("未来开发");
+        if(isset($_GET["r"]) && !empty($_GET)){
+            Session::delete("admin");
+        }else{
+            if (!Session::has("login")) {
+                header("Location: /user.php/login");
+                exit("未登录");
+            }
+            if (Db::table("icp_users")->where("id",Session::get("login"))->value("u_type") < 1) {
+                $this->error("用户权限不足","/user.php");
+            }
+            Session::set("admin",1);
+        }
+        return redirect("/user.php");
     }
 }
--- application/user/controller/Icp.php
+++ application/user/controller/Icp.php
@@ -14,7 +14,10 @@ class Icp extends Controller
             return '登录错误';
         }
         $this->assign("USER",Db::table("icp_users")->where("id",Session::get("login"))->find());
-        $this->assign("icp_list",Db::table("icp_list")->where("by_user",Session::get("login"))->select());
+        if(Session::has("admin"))
+            $this->assign("icp_list",Db::table("icp_list")->select());
+        else
+            $this->assign("icp_list",Db::table("icp_list")->where("by_user",Session::get("login"))->select());
         return $this->fetch("icp/index");
     }
     public function add(){
@@ -50,8 +53,18 @@ class Icp extends Controller
     }
     public function del($id){
         if (Session::has("login")) {
-            if(Db::table("icp_list")->where("icp_number",$id)->value("by_user") == Session::get("login")) Db::table("icp_list")->where("icp_number",$id)->update(["is_remove"=>1]);
+            if(Session::has("admin")) Db::table("icp_list")->where("icp_number",$id)->update(["is_remove"=>1]);
+            else{
+                if(Db::table("icp_list")->where("icp_number",$id)->value("by_user") == Session::get("login")) Db::table("icp_list")->where("icp_number",$id)->update(["is_remove"=>1]);
+                else return "no permissions";
+            }
+        }else return "login error";
+    }
+    public function re($id){
+        if (Session::has("login")) {
+            if(Session::has("admin")) Db::table("icp_list")->where("icp_number",$id )->update(["is_remove"=>0]);
             else return "no permissions";
         }else return "login error";
+        return $this->redirect("/user.php/icp");
     }
 }
